f2f93046e3453eec3b6872ad345823d91f4237da
Vittor Daniel <vittordaniel1108@gmail.com>
2022-07-13 21:29:28 -0300

    Projeto FInalizado

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import { MovieRow } from './components/MovieRow';
 import './App.css';
 import Tmdb from './Tmdb';
 import { FeaturedMovie } from './components/FeaturedMovie';
+import { Header } from './components/Header';
 
 export const App = () => {
 
   const [movieList, setMovieList] = React.useState([]);
   const [featuredData, setFeaturedData] = React.useState(null);
+  const [blackHeader, setBlackHeader] = React.useState(false);
+
   React.useEffect(() => {
 
     const loadAll = async () => {
@@ -28,13 +31,28 @@ export const App = () => {
 
   }, []);
 
+  React.useEffect(() => {
+
+    const scrollListenner = () =>{
+      if(window.scrollY > 10){
+        setBlackHeader(true);
+      }else{
+        setBlackHeader(false);
+      }
+    }
+
+    window.addEventListener('scroll', scrollListenner);
+    return () => {
+      window.removeEventListener('scroll', scrollListenner);
+    }
+  }, [])
 
 
   return (
     <div className="page">
+        <Header black={blackHeader}/>
 
       <section className="lists">
-
         {featuredData && <FeaturedMovie item={featuredData} />}
 
         {movieList.map((movie, key) => 
@@ -42,7 +60,17 @@ export const App = () => {
         )}
 
       </section>
-
+      
+      <footer>
+        Feito com <span role="img" arial-label="corãção">por Vittor Daniel ♥ </span><br/>
+        Direitos de imagem da Netlfix<br/>
+        Dados pego da Tmdb
+      </footer>
+      {movieList.length <= 0  &&
+        <div className='loading'>
+          <img src='https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif' alt='carregando'/>
+        </div>    
+      }
     </div>
   )
 }
